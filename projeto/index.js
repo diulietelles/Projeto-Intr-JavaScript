1c3506ebc647fb60bea415ea94bfdc0bf5d2bd24
Diulie Telles <121645164+diulietelles@users.noreply.github.com>
2023-09-11 11:44:56 -0300

    Add files via upload

--- projeto/index.js
+++ projeto/index.js
@@ -9,9 +9,7 @@ function cadastrar() {
     let idade = Number(prompt("Digite a idade do doador:"))
     let peso = Number(prompt("Digite o peso do doador (em kg):"))
     let tipoSanguineo = prompt("Digite o tipo sanguíneo do doador:")
-    let dataUltimaDoacao = Number(prompt("Digite a data da última doação (dd/mm/aaaa):"))
-    
-
+    let dataUltimaDoacao = prompt("Digite a data da última doação (dd/mm/aaaa):")
 
     let doador = {
         nome: nome,
@@ -22,18 +20,24 @@ function cadastrar() {
     }
 
     doadores.push(doador)
+
     console.log("Doador cadastrado com sucesso!")
+    console.log(doadores[doadores.length - 1])
 }
+
+
 // criar função para listar dados do doador//
 function listar() {
-    console.log("--------------------")
     console.log("LISTAGEM DE DOADORES:")
     console.log("--------------------")
-    console.log("NOME             | IDADE | PESO  | TIPO SANGUÍNEO | ÚLTIMA DOAÇÃO")
+    console.log("NOME | IDADE | PESO  | TIPO SANGUÍNEO | ÚLTIMA DOAÇÃO")
     console.log("---------------------------------------------------------------")
 
+    
 }
-// criar função para tipos sanguineos //
+
+
+//criar função para tipos sanguineos //
 function TipoSanguineo() {
     let tipoSanguineoDesejado = prompt("Digite o tipo sanguíneo desejado:")
     console.log("------------------------")
@@ -42,15 +46,9 @@ function TipoSanguineo() {
     console.log("NOME             | IDADE | PESO  | TIPO SANGUÍNEO | ÚLTIMA DOAÇÃO")
     console.log("---------------------------------------------------------------")
 }
-    for (let i = 0; i < doadores.length; i++) {
-        let doador = doadores[i]
-        if (doador.tipoSanguineo === tipoSanguineoDesejado) {
+    
         
-    console.log("---------------------------------------------------------------")
-}
-    }
-
-    // criar função para data da ultima doação do usuario//
+// criar função para data da ultima doação do usuario//
 function DataUltimaDoacao() {
     let dataDesejada = prompt("Digite a data desejada (dd/mm/aaaa):")
     console.log("------------------------")
@@ -59,28 +57,13 @@ function DataUltimaDoacao() {
     console.log("NOME             | IDADE | PESO  | TIPO SANGUÍNEO | ÚLTIMA DOAÇÃO")
     console.log("---------------------------------------------------------------")
 }
-    for (let i = 0; i < doadores.length; i++) {
-        let doador = doadores[i]
-        if (doador.dataUltimaDoacao < dataDesejada) {
-           
-        }
-    console.log("---------------------------------------------------------------")
-}
+
 // criar função sair//
 function sair() {
     continuar = false
     console.log("O programa foi encerrado.")
 }
 
-while (continuar) {
-    let pergunta = "===== SISTEMA DE CADASTRO DE DOADORES DE SANGUE =====\n"
-    pergunta += "1. Cadastrar doador\n"
-    pergunta += "2. Listar doadores\n"
-    pergunta += "3. Buscar doador por tipo sanguíneo\n"
-    pergunta += "4. Buscar doador por data da última doação\n"
-    pergunta += "5. Sair\n"
-
-}
 
 // Loop para perguntar ao usuário
 while (continuar) {
@@ -104,7 +87,7 @@ while (continuar) {
 		case 2:
 			listar()
 			break
-		case 3:
+		case buscar:
 			buscar()
 			break
 		case 4:
